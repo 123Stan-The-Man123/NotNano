368708bd93569caa594fd7b2aff70d63cf393862
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-09-28 10:46:14 +0100

    small changes

--- src/main.c
+++ src/main.c
@@ -2,13 +2,17 @@
 #include <termios.h>
 #include <unistd.h>
 
-int main(void) {
-    struct termios cooked_mode, raw_mode;
-    tcgetattr(STDIN_FILENO, &cooked_mode);
-    raw_mode = cooked_mode;
-    cfmakeraw(&raw_mode);
-    tcsetattr(STDIN_FILENO, TCSANOW, &raw_mode);
+#define MAXLINE 10000
 
+struct termios cooked_mode;
+struct termios raw_mode;
+
+void enable_raw_mode(void);
+void disable_raw_mode(void);
+
+int main(void) {
+    enable_raw_mode();
+    
     char c;
 
     do {
@@ -16,6 +20,22 @@ int main(void) {
         printf("You entered: %c\r\n", c);
     } while (c != 3);
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &cooked_mode);
+    disable_raw_mode();
     return 0;
+}
+
+void enable_raw_mode(void) {
+    tcgetattr(STDIN_FILENO, &cooked_mode);
+    raw_mode = cooked_mode;
+    raw_mode.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    raw_mode.c_oflag &= ~OPOST;
+    raw_mode.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    raw_mode.c_cflag &= ~(CSIZE | PARENB);
+    raw_mode.c_cflag |= CS8;
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw_mode);
+}
+
+void disable_raw_mode(void) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &cooked_mode);
 }
